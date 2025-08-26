a3555a2b3b99d084a384fe78a968c924f613ad8c
pocopota <pocopota.program@gmail.com>
2025-08-26 09:43:11 +0900

    ingest.tsでEXIF情報を取得し、Google GenAIを使用してキャプションを生成する機能を実装。

--- src/ingest.ts
+++ src/ingest.ts
@@ -1,13 +1,82 @@
 import { exiftool } from "exiftool-vendored";
-import { readdir } from "fs/promises";
+import { readdir, readFile } from "fs/promises";
 import path from "path";
+import { GoogleGenAI, createUserContent, Type } from "@google/genai";
+import { config } from "dotenv";
+config({ path: ".env.local" });
 
 // 該当写真一覧取得
 const names = await readdir("data/photos");
 
 // EXIF情報取得
+const exifs = [];
 for (const name of names) {
   const fullPath = path.join("data/photos", name);
   const tags = await exiftool.read(fullPath);
-  console.log(`${name}: ${tags.DateTimeOriginal}`);
+  exifs.push(tags);
 }
+
+// Google GenAIでキャプション生成
+if (!process.env.GEMINI_API_KEY) {
+  throw new Error("GEMINI_API_KEY is not set");
+}
+const ai = new GoogleGenAI({});
+
+const base64Images = [];
+for (const name of names) {
+  const fullPath = path.join("data/photos", name);
+  const imageBase64 = await readFile(fullPath, { encoding: "base64" });
+  base64Images.push(imageBase64);
+}
+
+const inlineDatas = [];
+for (const base64Image of base64Images) {
+  inlineDatas.push({
+    inlineData: {
+      data: base64Image,
+      mimeType: "image/jpeg",
+    },
+  });
+}
+
+interface CaptionResponse {
+  caption: string;
+}
+
+const response = await ai.models.generateContent({
+  model: "gemini-2.5-flash",
+  contents: createUserContent([
+    "以下の各画像に対して、簡潔なキャプションを日本語で生成してください。",
+    ...inlineDatas,
+  ]),
+  config: {
+    responseMimeType: "application/json",
+    responseSchema: {
+      type: Type.ARRAY,
+      items: {
+        type: Type.OBJECT,
+        properties: {
+          caption: { type: Type.STRING },
+        },
+      },
+    },
+  },
+});
+
+if(!response.text){
+  throw new Error("response.text is empty");
+}
+const parsedResponse = JSON.parse(response.text);
+
+// 写真データを統合したjsonの生成
+const photos = [];
+for (let i = 0; i < names.length; i++) {
+  photos.push({
+    id: i + 1,
+    fileName: "data/photos/" + names[i],
+    caption: parsedResponse[i].caption,
+    date: `${exifs[i]!.DateTimeOriginal}`
+  });
+}
+
+console.log(photos);
